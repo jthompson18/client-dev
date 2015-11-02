66edbf92445488706695523572e67b1953e62f35
John Thompson <johnthompson@4cinsights.com>
2015-11-01 22:00:42 -0600

    update app to use require.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+require.config({
+	baseUrl: 'js/', 
+	paths: {
+		jquery: [
+		'//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min',
+		// example for adding default in case http fails
+		'lib/jquery-2.1.4.min'
+		],
+		select2: '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min',
+		Highcharts: '//code.highcharts.com/highcharts',
+	},
+});
