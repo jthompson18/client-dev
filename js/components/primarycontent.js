1b6c120ea8db7309a296dfb1b0d61d8080d140d1
Paulo Chaves <paulochavesbr@gmail.com>
2015-11-20 16:13:33 -0600

    add components helptext and single select to primary content

--- js/components/primarycontent.js
+++ js/components/primarycontent.js
@@ -1,20 +1,41 @@
 const $ = require('jquery'),
       React = require('react'),
-      SectionHeading = require('./sectionheading.js');
+      SectionHeading = require('./sectionheading.js'),
+      HelpText = require('./helptext.js'),
+      SingleSelect = require('./singleselect.js');
 
 global.jQuery = $;
+
+var singleSelectOptions = [
+    {value:6 , label: "2013"},
+    {value:5 , label: "2012"},
+    {value:4 , label: "2011"},
+    {value:3 , label: "2010"},
+    {value:2 , label: "2008"},
+    {value:1 , label: "2005"},
+    {value:0 , label: "2000"}
+];
 
 var PrimaryContent = React.createClass({
     // TODO load data here as an event triggered by its children and pass the data as property ???
     getInitialState: function () {
         return {
             data: [],
-            selectedCountries: []
+            selectedCountries: [],
+            yearSelected: singleSelectOptions[0]
         };
     },
 
-    handleSelectDataChange: function (file, fieldName) {
-        console.log(file, fieldName);
+    handleSelectDataChange: function (data, fieldName) {
+        // for fieldName = select1 or select2, data = json filename
+        // for fieldName = select3, data = year selected
+        if (fieldName == 'select1' || fieldName == 'select2') {
+
+        } else if (fieldName == 'select3') {
+            // this.setState({yearSelected: data});
+        }
+
+        console.log(data, fieldName);
     },
 
     handleMultiSelectChange: function(selectedCountries) {
@@ -42,11 +63,28 @@ var PrimaryContent = React.createClass({
     },
 
     render: function () {
+        // TODO print help text according to data selected
+        var hdiHelpText2 = "The Human Development Index (HDI) is a composite index measuring average of the three basic metrics of human development: life expectancy, education rate, and average standard of living.";
+        var hdiInfoLink = "http://hdr.undp.org/en";
+
         return (
             <section id="content">
                 <SectionHeading
                     onSelectDataChange={this.handleSelectDataChange}
                     onMultiSelectChange={this.handleMultiSelectChange} />
+
+                <HelpText
+                    helpText={hdiHelpText2}
+                    externalLink={hdiInfoLink}  />
+
+                <section className="selector-left">
+                    <SingleSelect
+                        label="Select a year for a more in depth look: "
+                        options={singleSelectOptions}
+                        value={singleSelectOptions[0]}
+                        name="select3"
+                        onSelectChange={this.handleSelectDataChange} />
+                </section>
             </section>
         );
     },
