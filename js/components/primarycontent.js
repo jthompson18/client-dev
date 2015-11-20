59a557568bf9c905824e32930e8ad09c1e6ec087
Paulo Chaves <paulochavesbr@gmail.com>
2015-11-20 11:39:49 -0600

    add components wrapper, header, primary content and footer

---
+++ js/components/primarycontent.js
@@ -0,0 +1,13 @@
+const React = require('react');
+
+var PrimaryContent = React.createClass({
+    render: function () {
+        return (
+            <section id="content">
+                <h1>I am PrimaryContent!</h1>
+            </section>
+        );
+    }
+});
+
+module.exports = PrimaryContent;
